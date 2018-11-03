3382ab82e4bb0165572e2c280748dd09a8380f3d
yztazjin <1736635542@163.com>
2018-11-03 22:36:10 +0800

    plugin std activities

---
+++ AppPlugin/src/main/java/com/plugin/demo/plugin/ActivityPageOne.java
@@ -0,0 +1,28 @@
+package com.plugin.demo.plugin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityPageOne extends AppCompatActivity {
+
+    Button btn_next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        btn_next = findViewById(R.id.btn_next);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityPageOne.this,
+                        ActivityPageTwo.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
